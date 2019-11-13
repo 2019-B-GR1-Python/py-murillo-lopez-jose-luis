82cd1eda21ed934c98537f69c67d7865cff33546
Jose7198 <jose719810@hotmail.com>
2019-11-13 17:56:24 -0500

    Imagenes
    5 modificaciones a la imagen

---
+++ deber-rompecabezas/rompecabezas.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy import ndimage
+from scipy import misc
+import matplotlib.pyplot as plt
+
+pieces = input("Ingrese el número de piezas: ")
+
+mapache = misc.face()
+
+plt.imshow(mapache)
+plt.show()
+
+a = input()
+
+print(a)
