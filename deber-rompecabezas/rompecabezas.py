b4bcbde98a0064ed821e67560381485ed49ac5b7
Jose7198 <jose719810@hotmail.com>
2019-11-19 16:59:17 -0500

    deber rompecabezas

--- deber-rompecabezas/rompecabezas.py
+++ deber-rompecabezas/rompecabezas.py
@@ -4,6 +4,8 @@ from scipy import misc
 import matplotlib.pyplot as plt
 
 goal = misc.face()
+divisionY = 0
+divisionX = 0
 
 def getMinPrime(number, factor):
     if(number / factor == 1):
@@ -16,11 +18,8 @@ def getMinPrime(number, factor):
 def isCompleted(puzzle):
     return np.array_equal(goal, puzzle)
 
-def generatePuzzle(pieces):
-    divisionY = getMinPrime(int(pieces), 2)
-    divisionX = int(int(pieces) / divisionY)
+def cutPuzzle():
     vertical_slices = np.split(goal, divisionY)
-    np.random.shuffle(vertical_slices)
     handler = 0
     pieces_slices = np.zeros([int(pieces), int(goal.shape[0] / divisionY), int(goal.shape[1] / divisionX), 3], dtype=int)
     for slice in vertical_slices:
@@ -28,27 +27,52 @@ def generatePuzzle(pieces):
         for h_slice in horizontal_slices:
             pieces_slices[handler] = h_slice
             handler += 1
+    return pieces_slices
+
+def generatePuzzle(pieces):
+    global divisionX
+    global divisionY
+    divisionY = getMinPrime(int(pieces), 2)
+    divisionX = int(int(pieces) / divisionY)
+    pieces_slices = cutPuzzle()
     np.random.shuffle(pieces_slices)
     return rebuildPuzzle(pieces_slices)
 
 def rebuildPuzzle(pieces_slices):
-    divisionY = getMinPrime(pieces_slices.shape[0], 2)
-    divisionX = int(int(pieces) / divisionY)
     puzzle = np.zeros([divisionY, int(goal.shape[0] / divisionY), goal.shape[1], 3], dtype=int)
     for i in range(1, divisionY + 1):
         puzzle[i - 1] = np.concatenate(pieces_slices[(i -1) * divisionX: i * divisionX], 1)
     puzzle_final = np.concatenate(puzzle, 0)
     return puzzle_final
 
+def showPuzzle():
+    plt.figure(1)
+    plt.subplot(121)
+    plt.imshow(puzzle)
+    plt.subplot(122)
+    plt.imshow(goal)
+    plt.show()
+
+def movement_menu():
+    print("Seleccione a donde desea desplazar esta pieza:")
+    print("1. Arriba")
+    print("2. Abajo")
+    print("3. Izquierda")
+    print("4. Derecha")
+    return int(input("Seleccione una opción: "))
+
+def movement(piece, order):
+    puzzle = cutPuzzle()
+    showPuzzle()
+
+
 pieces = input("Ingrese el número de piezas: ")
 
 puzzle = generatePuzzle(pieces)
 
-#print(goal)
-
-plt.imshow(puzzle)
-plt.show()
-
-puzzle = goal
-
-print(isCompleted(puzzle))
+while(not isCompleted(puzzle)):
+    showPuzzle()
+    selected_piece = input(f"Seleccione una pieza [1 - {pieces}]: ")
+    selected_movement = movement_menu()
+    movement(selected_piece, selected_movement)
+    puzzle = goal
